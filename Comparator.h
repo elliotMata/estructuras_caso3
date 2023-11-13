f15ece74c24454f3f6aa16a3afad682b17793cb5
Steph Sandoval <stephsandovalc@gmail.com>
2023-11-12 21:40:16 -0600

    changes in comparator and match maker

--- Comparator.h
+++ Comparator.h
@@ -10,33 +10,48 @@ using namespace std;
 class Comparator
 {
 private:
-    int matches;
+    int matches, totalWords;
     double matchPercentage;
-    vector<string> base, target;
+    vector<string> base;
+    vector<pair<string, int>> target;
 
     void compare()
     {
         matches = 0;
-        for (const string &item : base)
+        totalWords = 0;
+
+        for (int i = 0; i < base.size(); i++)
+        {
+            for (int j = 0; j < target.size(); j++)
+            {
+                totalWords += target.at(j).second;
+                if (target.at(j).first.compare(base.at(i)))
+                {
+                    matches += target.at(j).second;
+                }
+            }
+        }
+
+        /*for (const string &item : base)
         {
             if (find(target.begin(), target.end(), item) != target.end())
             {
                 matches++;
             }
-        }
+        }*/
 
         matchPercentage = 0.0;
         if (target.size() > 0)
         {
-            matchPercentage = (static_cast<double>(matches) / target.size()) * 100.0;
+            matchPercentage = (static_cast<double>(matches) / totalWords) * 100.0;
         }
     }
 
 public:
-    void compareVectors(const vector<string> &vector1, const vector<string> &vector2)
+    void compareVectors(const vector<string> &vector1, const vector<pair<string, int>> &vector2)
     {
-        base = (vector1.size() <= vector2.size() ? vector1 : vector2);
-        target = (vector2.size() >= vector1.size() ? vector2 : vector1);
+        base = vector1;
+        target = vector2;
         compare();
     }
 
--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ json:
 	g++ JsonCreator.cpp NounReader.cpp FileReader.cpp -o json
 	
 # docker run -it --rm -v "F:\2023\Semestre 2\Estructuras de Datos\caso3:/home" -p 65000:65000 gcc bash
-# http://localhost:65000/match?req_phrase=love%20flower%20relationship%20dream
+# http://localhost:65000/match?req_phrase=horror%20monster%20dark%20terror
